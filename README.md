50d887de9ef4761926c2fed26f3f40e576d9cfe8
wrkbr <zcarc1@gmail.com>
2020-03-30 16:59:53 +0900

    #3.1 Create Account Resolver

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ Instagram clone with Express + Prisma + React and React Native
 
 ## User Stories
 
+- [ ] Log in
 - [ ] Like / Unlike a photo
 - [ ] Comment on a photo
 - [ ] Search by user
@@ -11,8 +12,7 @@ Instagram clone with Express + Prisma + React and React Native
 - [ ] Follow / Unfollow User
 - [ ] See the full photo
 - [ ] Edit my profile
-- [ ] Create account
-  - [ ] Log in
+- [x] Create account
 - [ ] Upload a photo
 - [ ] Edit the photo (Delete)
 - [ ] See the feed
---
+++ src/api/User/createAccount/createAccount.js
@@ -0,0 +1,20 @@
+import {prisma} from '../../../../generated/prisma-client/index';
+
+export default {
+    Mutation: {
+        createAccount: async (_, args) => {
+
+            const {username, email, firstName = "", lastName = "", bio = ""} = args;
+
+            const user = await prisma.createUser({
+              username,
+              email,
+              firstName,
+              lastName,
+              bio
+            });
+            
+            return user;
+        }
+    }
+}
