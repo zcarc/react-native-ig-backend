99cd8beb4dbe600e75466f1c16786195990830af
wrkbr <zcarc1@gmail.com>
2020-04-01 19:12:48 +0900

    #3.14 Computed Fields in Prisma

--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -4,21 +4,42 @@ import { prisma } from "../../../../generated/prisma-client";
 // (_) 언더스코어는 변수 이름이 될 수 있다.
 // (__) 더블언더스코어는 부모의 arguments를 뜻한다. (부모의 arguments === args)
 // 그래서 더블언더스코어 자리에 arguments를 넣어줘도 된다.
+
+// (_) 이건 root나 parent로 불린다.
 export default {
   Query: {
     me: async (_, __, { request, isAuthenticated }) => {
+      console.log("user parent: ", _);
       isAuthenticated(request);
-      const {user} = request;
-      const userProfile = await prisma.user({id: user.id});
-      const posts = await prisma.user({id: user.id}).posts();
+      const { user } = request;
+      const userProfile = await prisma.user({ id: user.id });
+      const posts = await prisma.user({ id: user.id }).posts();
 
       return {
-          user: userProfile,
-          posts
-      }
+        user: userProfile,
+        posts
+      };
 
       // Prisma client는 강한 관계를 설정하지 않으므로 ".$fragment()"를 붙여줘서 posts들을 가져올 수 있다.
-      // return prisma.user({id: user.id}).$fragment(USER_FRAGMENT); 
+      // return prisma.user({id: user.id}).$fragment(USER_FRAGMENT);
+    }
+  },
+
+  User: {
+
+    fullName: parent => {
+
+      return `${parent.firstName} ${parent.lastName}`;
     }
   }
+
+  // fullName의 쿼리상의 부모는 User이므로 parent는 User가 반환됨
+  // Query me의 return { user: userProfile , posts}
+  // 이 부분이  user: null 이라면 parent는 null이 반환됨
+  // User: {
+  //   fullName: (parent, __, { request }) => {
+  //     console.log('parent of', parent);
+  //     return "lalalal";
+  //   }
+  // }
 };
