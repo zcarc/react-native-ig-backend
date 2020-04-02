f0986bccb1b1d3c31e72a7a220c81c188ec481fe
wrkbr <zcarc1@gmail.com>
2020-04-03 00:30:06 +0900

    #3.25 seeRoom Resolver

--- src/api/User/editUser/editUser.js
+++ src/api/User/editUser/editUser.js
@@ -5,7 +5,7 @@ export default {
       // async 없어도 동작된다.
     editUser: (_, args, { request, isAuthenticated }) => {
       isAuthenticated(request);
-      const { username, email, firstName, lastName, bio } = args;
+      const { username, email, firstName, lastName, bio, avatar } = args;
       const {user} = request;
 
       // 여기서 리턴할 때 await를 붙이지 않아도 되는 이유는
@@ -13,9 +13,9 @@ export default {
       // 자동으로 이 promise가 resolve되서 브라우저에게 결과를 전달하길 기다려줘서
       // await를 사용해서 return 할 필요가 없다.
       return prisma.updateUser({
-          where: {id: user.id},
-          data: {username, email, firstName, lastName, bio}
-      })
+        where: { id: user.id },
+        data: { username, email, firstName, lastName, bio, avatar }
+      });
     }
   }
 };
--- src/fragments.js
+++ src/fragments.js
@@ -3,6 +3,7 @@
 export const USER_FRAGMENT = `
         id
         username
+        avatar
 `;
 
 export const COMMENT_FRAGMENT = `
@@ -35,11 +36,25 @@ export const FULL_POST_FRAGMENT = `
     }
 `;
 
+export const MESSAGE_FRAGMENT = `
+    id
+    text
+    to {
+        ${USER_FRAGMENT}
+    }
+    from {
+        ${USER_FRAGMENT}
+    }
+`;
+
 export const ROOM_FRAGMENT = `
     fragment RoomParts on Room {
         id
         participants {
-            id
+            ${USER_FRAGMENT}
+        }
+        messages{
+            ${MESSAGE_FRAGMENT}
         }
     }
 `;
