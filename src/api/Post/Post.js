e57894d46f60ff0590092a2785bf18a8ffc4599f
wrkbr <zcarc1@gmail.com>
2020-04-03 16:25:26 +0900

    #3.30 Kill The Fragments

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -2,6 +2,9 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Post: {
+    files: ({ id }) => prisma.post({ id }).files(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
     isLiked: (parent, _, { request }) => {
       const { user } = request;
       const { id } = parent;
@@ -27,6 +30,6 @@ export default {
           where: { post: { id: parent.id } }
         })
         .aggregate()
-        .count()
+        .count(),
   }
 };
