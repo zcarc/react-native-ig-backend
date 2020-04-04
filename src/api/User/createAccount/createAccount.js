bf6e3feb3ecddddfd933eaef6e527cdd23e40d65
wrkbr <zcarc1@gmail.com>
2020-04-05 05:48:44 +0900

    #5.6 createAccount Mutation part Two

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -1,3 +1,5 @@
+import { prisma } from "../../../../generated/prisma-client";
+
 export default {
   Mutation: {
     // prisma를 3번째 인자인 context에서 가져온다.
@@ -5,6 +7,11 @@ export default {
     createAccount: async (_, args) => {
       
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
+
+      const exists = await prisma.$exists.user({username});
+      if(exists) {
+        throw Error("This username is already taken");
+      }
 
       const user = await prisma.createUser({
         username,
