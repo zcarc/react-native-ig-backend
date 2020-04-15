b8eb5454ec0a25df7c53b9da141261956edb173a
wrkbr <zcarc1@gmail.com>
2020-04-15 16:45:58 +0900

    #18.7 Preparing for Upload

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import schema from './schema';
 import "./passport";
 import { authenticateJwt } from './passport';
 import { isAuthenticated } from './middlewares';
+import { uploadMiddleware, uploadController } from "./upload";
 
 const PORT = process.env.PORT || 4000;
 
@@ -26,6 +27,7 @@ const server = new GraphQLServer({
 
 server.express.use(logger('dev'));
 server.express.use(authenticateJwt);
+server.express.post("/api/upload", uploadMiddleware, uploadController);
 
 server.start({ port: PORT }, () =>
   console.log(`✔ Server running on http://localhost:${PORT}`)
